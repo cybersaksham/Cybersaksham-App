b957e6f462e4194237f3b5e4081b2452def3fd7d
Saksham <2020UCP1801@mnit.ac.in>
2021-10-11 14:10:45 +0530

    version 2.0

--- app/src/main/java/com/cybersaksham/portfolio/MainActivity.java
+++ app/src/main/java/com/cybersaksham/portfolio/MainActivity.java
@@ -2,19 +2,60 @@ package com.cybersaksham.portfolio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+    private void startWebView(WebView webView, String url) {
+        WebSettings settings = webView.getSettings();
+
+        settings.setJavaScriptEnabled(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_OUTSIDE_OVERLAY);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+
+        ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                progressDialog.show();
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                if (progressDialog.isShowing()) {
+                    progressDialog.dismiss();
+                }
+            }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(MainActivity.this, "Error:" + description, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        webView.loadUrl(url);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WebView webView = findViewById(R.id.webview);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setUseWideViewPort(true);
-        webView.getSettings().setLoadWithOverviewMode(true);
-        webView.loadUrl("https://cybersaksham.herokuapp.com/");
+        WebView myWebView = findViewById(R.id.webview);
+        startWebView(myWebView, "https://cybersaksham.herokuapp.com/");
     }
 }
